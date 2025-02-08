0d5ad92094d2b4f7eb52320985a9f908b6247df8
rec0de <mail@rec0de.net>
2025-02-08 17:05:29 +0100

    split stars, tweaked init doc, use opus sounds

--- zip-the-game.sh
+++ zip-the-game.sh
@@ -1,1 +1,1 @@
-zip -r ../matsutake-$(date -u +%Y-%m-%dT%H_%M_%S%Z).zip index.html controls.js tween.js COPYING aud img
+zip -r ../matsutake-$(date -u +%Y-%m-%dT%H_%M_%S%Z).zip index.html controls.js tween.js COPYING aud/*.opus img
